2d4ed2a58675fd200a43cf9584dc27fa542928f6
majewskimateusz <kontakt@mateuszmajewski.com>
2021-01-27 15:53:59 +0100

    Usunięcie dokumentacji ze źródła projektu

--- dokumentacja RhymersFactory /package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.kis.vh.nursery.factory"}];updateSearchResults();
--- dokumentacja RhymersFactory /type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"edu.kis.vh.nursery.factory","l":"RhymersFactory"}];updateSearchResults();
